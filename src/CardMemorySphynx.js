ecef28001c39db2c2ac5fa5d180daff2a992daac
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-03-23 14:06:08 -0600

    Logic for a picked card

--- src/CardMemorySphynx.js
+++ src/CardMemorySphynx.js
@@ -11,12 +11,35 @@ export class CardMemorySphynx extends LitElement {
             width:100%;
             border-radius:22px;
           }
+          #card{
+            height:100%;
+            width:100%;
+            background: rgba( 255, 255, 255, 0.10 );
+            box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+            backdrop-filter: blur( 4px );
+            -webkit-backdrop-filter: blur( 4px );
+            border-radius: 22px;
+            border: 1px solid rgba( 255, 255, 255, 0.18 );
+          }
+          #card:hover{
+            -webkit-box-shadow: 0px 0px 20px 1px #000000; 
+            box-shadow: 0px 0px 20px 1px #000000;
+          }
+          #card.matched{
+            display:none;
+          }
+          .picked{
+            display:none;
+          }
         `;
     }
-    render() {
+   
+    render() { this.picked=false;
         return html`
-        <img src="../assets/img/Picture1.png">
-
+        <div id="card" class="${this.played ? 'matched' : ''}">
+        <img class="${this.picked ? '' :'picked'}"src="../assets/img/Picture1.png">
+        </div>
+        
         `;
     }
 }
